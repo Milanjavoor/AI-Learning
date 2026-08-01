3c8c15af383763a4bc7b27a15a822df84eb94d65
Milan Javoor <25bda065@iiitdwd.ac.in>
2026-08-01 12:48:45 +0530

    Add PyTorch embedding example to Encoder.py

--- Encoder.py
+++ Encoder.py
@@ -1,1 +1,8 @@
-
+import torch 
+import torch.nn as nn
+vocab_size=50000
+dim=512
+embedding=nn.Embedding(vocab_size,dim)
+token_ids=torch.tensor([15,284,9123])
+embedded=embedding(token_ids)
+print(embedded.shape)
